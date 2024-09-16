4b5bcb649d257cc16d99d253fb2a76ce30566ac4
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2024-09-16 22:52:33 +0530

    db connection established

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import connectDB from './src/db/db.js';
+
+dotenv.config({
+    path:'.env'
+})
+
 const app = express();
 app.get('/',(req,res)=>{
     res.send("Bapi")
 })
+
 app.listen(3000,()=>{
+    connectDB()
     console.log('Server listening');
 })
