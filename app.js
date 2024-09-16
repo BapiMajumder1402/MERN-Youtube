b45fd3705e620d26f947ab00f2e0013d62999ecd
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2024-09-17 00:50:48 +0530

    error and response handler added

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 import dotenv from 'dotenv';
-import express from 'express';
+import express, { urlencoded } from 'express';
 import connectDB from './src/db/db.js';
+import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 dotenv.config({
     path:'.env'
 })
 
 const app = express();
+app.use(cors({origin:process.env.CORS}));
+app.use(cookieParser())
+app.use(express.json({limit:"50kb"}))
+app.use(express.urlencoded({extended:true, limit:"50kb"}));
+app.use(express.static("public"))
+
+
+
+
+
+
+
 app.get('/',(req,res)=>{
     res.send("Bapi")
 })
 
-app.listen(3000,()=>{
-    connectDB()
-    console.log('Server listening');
-})
+connectDB().then(()=>{
+    app.listen(process.env.PORT  || 8000,()=>{
+        console.log(`Server listening on port ${process.env.PORT}` );
+    })
+}).catch((error)=>{
+    console.log(`Server error: ${error}`);}
+)
