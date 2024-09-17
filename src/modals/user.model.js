a4f8fc16a2e76f1ac526595ba8a30899c39593d6
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2024-09-17 16:09:48 +0530

    pre hook and bcrypt added

--- src/modals/user.model.js
+++ src/modals/user.model.js
@@ -1,5 +1,5 @@
 import mongoose ,{Schema} from "mongoose";
-
+import bcrypt from "bcrypt";
 const userSchema =new Schema({
     fullName:{
         type:String,
@@ -40,5 +40,15 @@ const userSchema =new Schema({
         required:true,
     },
 },{timestamps:true})
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password,10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
 
 export const User= mongoose.model("User",userSchema)
