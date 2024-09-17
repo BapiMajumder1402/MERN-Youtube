8c3cf266b076901e219ce5c4f629717019705cc9
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2024-09-17 18:03:15 +0530

    jwt and refresh token generation added

--- src/modals/user.model.js
+++ src/modals/user.model.js
@@ -1,54 +1,75 @@
-import mongoose ,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
-const userSchema =new Schema({
-    fullName:{
-        type:String,
-        required:true,
-        index:true
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+        index: true
     },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        lowercase:true
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
     },
-    userName:{
-        type:String,
-        required:true,
-        index:true
+    userName: {
+        type: String,
+        required: true,
+        index: true
     },
-    avatar:{
-        type:String,
-        required:true,
+    avatar: {
+        type: String,
+        required: true,
     },
-    coverImage:{
-        type:String,
-        required:true,
+    coverImage: {
+        type: String,
+        required: true,
     },
-    watchHistory:[
+    watchHistory: [
         {
-            type:Schema.Types.ObjectId,
-            ref:"Video"
+            type: Schema.Types.ObjectId,
+            ref: "Video"
         }
     ],
-    password:{
-        type:String,
-        required:[true,"Password Required"],
+    password: {
+        type: String,
+        required: [true, "Password Required"],
     },
-    refreshToken:{
-        type:String,
-        required:true,
+    refreshToken: {
+        type: String,
+        required: true,
     },
-},{timestamps:true})
+}, { timestamps: true })
 
-userSchema.pre("save", async function(next){
-    if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password,10);
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10);
     next();
 })
-
+// ACCESS_TOKEN_SECRET
+// REFRESH_TOKEN_SECRET EXPIRY
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password,this.password)
+    return await bcrypt.compare(password, this.password)
+}
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        userName: this.userName
+    }, process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        })
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id,
+    }, process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        })
 }
 
-export const User= mongoose.model("User",userSchema)
+export const User = mongoose.model("User", userSchema)
